59d948cecf3b76e6461fd1d2d7cb19469979d229
Triphon Penakov <triphon@gmail.com>
2020-03-21 10:59:22 +0200

    Initial commit

---
+++ src/test/java/org/otaibe/nginx/with/eureka/demo/eureka/server/OtaibeNginxWithEurekaDemoEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.otaibe.nginx.with.eureka.demo.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtaibeNginxWithEurekaDemoEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
